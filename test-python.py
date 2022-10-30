b5835a69db758ad9d359b606a50a1b555c3e4e91
Matias Caccavo <caccavomatias@gmail.com>
2022-10-30 00:30:25 -0300

    initial

--- test-python.py
+++ test-python.py
@@ -5,3 +5,5 @@ print("Hoy es Sabado 29/10/2022 y son las 23:34")
 print("Pruebas iniciales de commits python")
 print(26*25)
 print("Cambio de las 00:20")
+
+print("Nuevo cambio 00:27 para pobar sincronización")
